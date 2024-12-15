9e96edb717fb02797d24f8d2604d01907f09006e
VLDrenth <laszlodrenth@gmail.com>
2024-12-15 15:18:57 +0000

    First part of first post

---
+++ _pages/about.md
@@ -0,0 +1,6 @@
+---
+permalink: /about/
+title: "About"
+---
+
+This blog is made for me to learn in public about topics that interest me and motivate myself to actually write out what I learn. Topics that interest me are statistical learning theory, probabilistic programming, time-series analysis, and deep learning. Expect butchered interpretations of classic textbooks, discussion of papers far beyond my comprehension, and other fun stuff!
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "LASSO and some variants",
+        "excerpt":"The basics One of the first ‘machine learning’ many will come across is LASSO regression. This may suggest that it’s an elementary method; something to get you started on machine learning before moving to the more interesting topics. But there is a sea of methods that are adaptions of LASSO....","categories": ["Machine Learning"],
+        "tags": ["machine-learning","lasso","regularization","statistics","regression"],
+        "url": "/machine%20learning/2024/12/03/lasso1.html",
+        "teaser": null
+      }]
